612d994cf9bd478b08b47c5f7de7466a01fc4074
xojld0 <34447743+xojld0@users.noreply.github.com>
2021-06-23 19:39:34 +0200

    Add files via upload

--- Engine/Hoopoe.h
+++ Engine/Hoopoe.h
@@ -1,6 +1,7 @@
 #ifndef __ENGINE_HOOPOE_H__
 #define __ENGINE_HOOPOE_H__
 
+#include "Core/include/Exception.h"
 #include "Core/include/Application.h"
 #include "Core/include/LogManager.h"
 #include "Core/include/Database.h"
