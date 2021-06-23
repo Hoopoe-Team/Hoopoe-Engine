0ae5d3371c2da13636e1210a0b1ccb33b388ad91
xojld0 <34447743+xojld0@users.noreply.github.com>
2021-06-23 17:59:13 +0200

    Add files via upload

--- Engine/UTest/src/UTest.cpp
+++ Engine/UTest/src/UTest.cpp
@@ -4,8 +4,8 @@ int main(int argc, char* argv[])
 {
     if (argc > 1) {
         for(int i = 1; i < argc; ++i) {
-            std::string command = std::string("./") + std::string("utests/") + std::string(argv[i]);
-            system(argv[i]);
+            std::string command = std::string("./") + std::string("utests/") + std::string("Test") + std::string(argv[i]);
+            system(command.c_str());
         }
     } else {
         system("./utests/*");
