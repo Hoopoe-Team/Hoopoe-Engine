b5a04d4f013aa6849c0a83c40d587c8ff0b0dd3d
xojld0 <34447743+xojld0@users.noreply.github.com>
2021-06-27 23:15:07 +0200

    Added FileSystem
    UTESTS: 5/13

--- Engine/Core/include/FileSystem.h
+++ Engine/Core/include/FileSystem.h
@@ -4,6 +4,10 @@
 #include <fstream>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ftw.h>
 
 #include "CoreHeaders.h"
 #include "DataStream.h"
@@ -14,8 +18,28 @@ namespace Hoopoe
 class FileSystem : public FileSystemAlloc
 {
 public:
-    static DataStreamPtr openFileStream(const String& path, std::ios::openmode mode, const String& name = "");
+    // Files
+    static DataStreamPtr openFileStream(const String &path, std::ios::openmode mode, const String &name = "");
+    
+    static bool createFile(const String &fileName);   
+    static bool deleteFile(const String &fileName);
+    static bool moveFile(const String &oldPath, const String &newPath);
+    static bool isFile(const String &path);
+    static size_t getFileSize(const String &path);
 
+    // Folders
+    static String getFullPath(const String &path);
+    static bool createFolder(const String &path);
+    static bool deleteEmptyFolder(const String &folderPath);
+    static bool deleteFolderContents(const String &folderPath);
+    static bool deleteFolderAndContents(const String &folderPath);
+    static bool moveFolder(const String &oldPath, const String &newPath);
+    static bool copyFolder(const String &oldPath, const String &newPath);
+    static bool isFolder(const String &path);
+    static String getCurrentDirectory();
+
+private:
+    static struct stat pathStat;
 }; // FileSystem
 
 } // Hoopoe
--- Engine/Core/src/FileSystem.cpp
+++ Engine/Core/src/FileSystem.cpp
@@ -3,17 +3,18 @@
 namespace Hoopoe
 {
 
-DataStreamPtr FileSystem::openFileStream(const String& fullPath, std::ios::openmode mode, const String& name)
+struct stat FileSystem::pathStat;
+
+DataStreamPtr FileSystem::openFileStream(const String &fullPath, std::ios::openmode mode, const String &name)
 {
-    struct stat tagStat;
-    int ret = stat(fullPath.c_str(), &tagStat);
-    size_t st_size = ret == 0 ? tagStat.st_size : 0;
+    int ret = stat(fullPath.c_str(), &pathStat);
+    size_t st_size = ret == 0 ? pathStat.st_size : 0;
 
     std::istream* baseStream = 0;
     std::ifstream* roStream = 0;
     std::fstream* rwStream = 0;
 
-    if (mode & std::ios::out)
+    if (mode &std::ios::out)
     {
         rwStream = HE_NEW_T(std::fstream);
         rwStream->open(fullPath.c_str(), mode);
@@ -27,7 +28,6 @@ DataStreamPtr FileSystem::openFileStream(const String& fullPath, std::ios::openm
 
         baseStream = roStream;
     }
-
 
     // Should check ensure open succeeded, in case fail for some reason.
     if (baseStream->fail())
@@ -48,11 +48,106 @@ DataStreamPtr FileSystem::openFileStream(const String& fullPath, std::ios::openm
     else
     {
         HE_ASSERT(ret == 0, "Problem getting file size");
+
         // read-only stream
         stream = new FileStream(streamname, roStream, st_size);
     }
 
     return DataStreamPtr(stream);
+}
+
+// Only for UNIX
+String FileSystem::getFullPath(const String &path)
+{
+    return String(FileSystem::getCurrentDirectory() + String("/") + path);
+}
+
+bool FileSystem::createFile(const String &fileName)
+{
+    return creat(fileName.c_str(), S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) != -1;
+}
+
+bool FileSystem::deleteFile(const String &fileName)
+{
+    return remove(fileName.c_str()) != -1;
+}
+
+bool FileSystem::moveFile(const String &oldPath, const String &newPath)
+{
+    return rename(oldPath.c_str(), newPath.c_str()) != -1;
+}
+
+bool FileSystem::isFile(const String &path)
+{
+    stat(path.c_str(), &pathStat);
+
+    return S_ISREG(pathStat.st_mode);
+}
+
+size_t FileSystem::getFileSize(const String &path)
+{
+    stat(path.c_str(), &pathStat);
+
+    return pathStat.st_size;
+}
+
+bool FileSystem::createFolder(const String &path)
+{
+    if (stat(path.c_str(), &pathStat) == -1)
+        return mkdir(path.c_str(), 0700) != -1;
+
+    return 0;
+}
+
+bool FileSystem::deleteEmptyFolder(const String &folderPath)
+{
+    return rmdir(folderPath.c_str()) != -1;
+}
+
+bool FileSystem::deleteFolderContents(const String &folderPath)
+{
+    //nftw(folder.c_str());
+    //unlink(folder.c_str());
+
+    //HE_EXCEPT(Exception::ERR_NOT_IMPLEMENTED, "This code doesn't implemented.", "FileSystem::deleteFolderContents");
+
+    return 0;
+}
+
+bool FileSystem::deleteFolderAndContents(const String &folderPath)
+{
+    return deleteFolderContents(folderPath) && deleteEmptyFolder(folderPath);
+}
+
+bool FileSystem::moveFolder(const String &oldPath, const String &newPath)
+{
+    //HE_EXCEPT(Exception::ERR_NOT_IMPLEMENTED, "This code doesn't implemented.", "FileSystem::moveFolder");
+
+    return 0;
+}
+
+bool FileSystem::copyFolder(const String &oldPath, const String &newPath)
+{
+    //HE_EXCEPT(Exception::ERR_NOT_IMPLEMENTED, "This code doesn't implemented.", "FileSystem::copyFolder");
+
+    return 0;
+}
+
+bool FileSystem::isFolder(const String &path)
+{
+    stat(path.c_str(), &pathStat);
+
+    return S_ISDIR(pathStat.st_mode);
+}
+
+String FileSystem::getCurrentDirectory()
+{
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        return String(cwd);
+    }
+
+    HE_EXCEPT(Exception::ERR_ITEM_NOT_FOUND, "Cannot get current directory", "FileSystem::getCurrentDirectory()");
 }
 
 } // Hoopoe
