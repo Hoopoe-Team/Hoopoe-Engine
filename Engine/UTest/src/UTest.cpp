188bedae0c94032b22784b1f128f5f872a911b5c
xojld0 <34447743+xojld0@users.noreply.github.com>
2021-07-06 01:26:11 +0200

    Add files via upload

--- Engine/UTest/src/UTest.cpp
+++ Engine/UTest/src/UTest.cpp
@@ -1,21 +1,38 @@
 #include <dirent.h>
+#include <libgen.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <iostream>
+#include <limits>
+#include <stdlib.h>
 
 #include "../../Core/include/Types.h"
 
 int main(int argc, char* argv[]) 
 {
+    char buff[PATH_MAX];
+    ssize_t len = readlink("/proc/self/exe", buff, sizeof(buff)-1);
+    if (len == -1) {
+        std::cout << "Failed to get absolute path of utests.\n";
+        return -1;
+    }
+
+    buff[len] = '\0';
+    Hoopoe::String utestsPath = Hoopoe::String(dirname(buff)) + "/utests/";
+
     if (argc > 1) 
     {
         for(int i = 1; i < argc; ++i) 
         {
-            Hoopoe::String command = Hoopoe::String("./") + Hoopoe::String("utests/") + Hoopoe::String("Test") + Hoopoe::String(argv[i]);
+            Hoopoe::String command = utestsPath + Hoopoe::String("Test") + Hoopoe::String(argv[i]);
             system(command.c_str());
         }
     } 
     else // runs all tests from utests directory. (only for Linux)
     {
-        DIR *dir = opendir("./utests/");
+        DIR *dir = opendir(utestsPath.c_str());
 
         if(dir != NULL)
         {
@@ -23,7 +40,9 @@ int main(int argc, char* argv[])
             while((ent = readdir(dir)) != NULL)
             {
                 if(ent->d_type != DT_DIR)
-                    system(Hoopoe::String(Hoopoe::String("./utests/") + Hoopoe::String(ent->d_name)).c_str());
+                {
+                    system(Hoopoe::String(utestsPath + Hoopoe::String(ent->d_name)).c_str());
+                }
             }
         }
         else
