c42a9a81a229bfa5eb9b043094bdcc6e75daa4d0
xojld0 <34447743+xojld0@users.noreply.github.com>
2021-07-11 16:31:00 +0200

    Add files via upload

--- Engine/Hoopoe.h
+++ Engine/Hoopoe.h
@@ -1,6 +1,7 @@
 #ifndef __ENGINE_HOOPOE_H__
 #define __ENGINE_HOOPOE_H__
 
+#include "Core/include/Platform.h"
 #include "Core/include/SharedPtr.h"
 #include "Core/include/Memory.h"
 #include "Core/include/Exception.h"
@@ -8,8 +9,6 @@
 #include "Core/include/ConfigFile.h"
 #include "Core/include/Application.h"
 #include "Core/include/LogManager.h"
-
-#include "Renderer/include/Graphics.h"
 
 #include "Core/src/Entry.h"
 
