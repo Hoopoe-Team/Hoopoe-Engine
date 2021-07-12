9b247e2e64d1b61c64be1c9986b4bc92292598de
xojld0 <34447743+xojld0@users.noreply.github.com>
2021-07-12 23:30:06 +0200

    Add files via upload

--- Engine/Core/test/include/TestFileSystem.h
+++ Engine/Core/test/include/TestFileSystem.h
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (C) 2020-2021 Xios
+ *
+ *  This file is part of Hoopoe-Engine.
+ *
+ *  Hoopoe-Engine is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Hoopoe-Engine is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Hoopoe-Engine.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 #ifndef __HOOPOE_ENGINE_CORE_TEST_FILESYSTEM_H__
 #define __HOOPOE_ENGINE_CORE_TEST_FILESYSTEM_H__
 
@@ -9,7 +29,7 @@ namespace Hoopoe { namespace Test {
 class TestFileSystem : public TestBase
 {
 public:
-    TestFileSystem(const String name) : TestBase(name) {}
+    TestFileSystem(const String& name) : TestBase(name) {}
     ~TestFileSystem() {}
 
     void run() override;
@@ -24,8 +44,8 @@ private:
     void testDeleteEmptyFolder();
     void testDeleteFolderContents();
     void testDeleteFolderAndContents();
-    void testMoveFolder();
-    void testCopyFolder();
+    void testIsAbsolutePath();
+    void testCatPath();
     void testIsFolder();
     void testGetCurrentDirectory();
 };
--- Engine/Core/test/src/TestFileSystem.cpp
+++ Engine/Core/test/src/TestFileSystem.cpp
@@ -1,3 +1,23 @@
+/*
+ *  Copyright (C) 2020-2021 Xios
+ *
+ *  This file is part of Hoopoe-Engine.
+ *
+ *  Hoopoe-Engine is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Hoopoe-Engine is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Hoopoe-Engine.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 #include "../include/TestFileSystem.h"
 
 namespace Hoopoe { namespace Test {
@@ -13,75 +33,75 @@ void TestFileSystem::run()
     testDeleteEmptyFolder();
     testDeleteFolderContents();
     testDeleteFolderAndContents();
-    testMoveFolder();
-    testCopyFolder();
+    testIsAbsolutePath();
+    testCatPath();
     testIsFolder();
     testGetCurrentDirectory();
 }
 
 void TestFileSystem::testCreateFile()
 {
-    UTEST_ASSERT(FileSystem::createFile(FileSystem::getFullPath("resources/TestFileSystem/testCreateFile")));
+    HE_UTEST_ASSERT(FileSystem::createFile(FileSystem::getFullPath("resources/TestFileSystem/testCreateFile")));
 }
 
 void TestFileSystem::testDeleteFile()
 {
-    UTEST_ASSERT(FileSystem::deleteFile(FileSystem::getFullPath("resources/TestFileSystem/testDeleteFile")));
+    HE_UTEST_ASSERT(FileSystem::deleteFile(FileSystem::getFullPath("resources/TestFileSystem/testDeleteFile")));
 }
 
 void TestFileSystem::testMoveFile()
 {
-    UTEST_ASSERT(FileSystem::moveFile(FileSystem::getFullPath("resources/TestFileSystem/dir1/testMoveFile"), FileSystem::getFullPath("resources/TestFileSystem/dir2/testMoveFile")));
+    HE_UTEST_ASSERT(FileSystem::moveFile(FileSystem::getFullPath("resources/TestFileSystem/dir1/testMoveFile"), FileSystem::getFullPath("resources/TestFileSystem/dir2/testMoveFile")));
 }
 
 void TestFileSystem::testIsFile()
 {
-    UTEST_ASSERT(FileSystem::isFile(FileSystem::getFullPath("resources/TestFileSystem/testIsFile")));
+    HE_UTEST_ASSERT(FileSystem::isFile(FileSystem::getFullPath("resources/TestFileSystem/testIsFile")));
 }
 
 void TestFileSystem::testGetFileSize()
 {
-    UTEST_ASSERT(FileSystem::getFileSize(FileSystem::getFullPath("resources/TestFileSystem/testGetFileSize")) == 9);
+    HE_UTEST_ASSERT(FileSystem::getFileSize(FileSystem::getFullPath("resources/TestFileSystem/testGetFileSize")) == 9);
 }
 
 void TestFileSystem::testCreateFolder()
 {
-    UTEST_ASSERT(FileSystem::createFolder(FileSystem::getFullPath("resources/TestFileSystem/testCreateFolder")));
+    HE_UTEST_ASSERT(FileSystem::createFolder(FileSystem::getFullPath("resources/TestFileSystem/testCreateFolder")));
 }
 
 void TestFileSystem::testDeleteEmptyFolder()
 {
-    UTEST_ASSERT(FileSystem::deleteEmptyFolder(FileSystem::getFullPath("resources/TestFileSystem/testDeleteEmptyFolder")));
+    HE_UTEST_ASSERT(FileSystem::deleteEmptyFolder(FileSystem::getFullPath("resources/TestFileSystem/testDeleteEmptyFolder")));
 }
 
 void TestFileSystem::testDeleteFolderContents()
 {
-    UTEST_ASSERT(FileSystem::deleteFolderContents(FileSystem::getFullPath("resources/TestFileSystem/testDelDirContents")));
+    HE_UTEST_ASSERT(FileSystem::deleteFolderContents(FileSystem::getFullPath("resources/TestFileSystem/testDelDirContents")));
 }
 
 void TestFileSystem::testDeleteFolderAndContents()
 {
-    UTEST_ASSERT(FileSystem::deleteFolderAndContents(FileSystem::getFullPath("resources/TestFileSystem/testDelDirAndContents")));
+    HE_UTEST_ASSERT(FileSystem::deleteFolderAndContents(FileSystem::getFullPath("resources/TestFileSystem/testDelDirAndContents")));
 }
 
-void TestFileSystem::testMoveFolder()
+void TestFileSystem::testIsAbsolutePath()
 {
-    UTEST_ASSERT(FileSystem::moveFolder(FileSystem::getFullPath("resources/TestFileSystem/dir1/dir3"), FileSystem::getFullPath("resources/TestFileSystem/dir2/dir3")));
+    HE_UTEST_ASSERT(FileSystem::isAbsolutePath("/usr/lib"));
 }
 
-void TestFileSystem::testCopyFolder()
+void TestFileSystem::testCatPath()
 {
-    UTEST_ASSERT(FileSystem::copyFolder(FileSystem::getFullPath("resources/TestFileSystem/dir1"), FileSystem::getFullPath("resources/TestFileSystem/dir4")));
+    HE_UTEST_ASSERT(FileSystem::catPath("/usr", "lib") == "/usr/lib");
 }
 
 void TestFileSystem::testIsFolder()
 {
-    UTEST_ASSERT(FileSystem::isFolder(FileSystem::getFullPath("resources/TestFileSystem/dir1")));
+    HE_UTEST_ASSERT(FileSystem::isFolder(FileSystem::getFullPath("resources/TestFileSystem/dir1")));
 }
 
 void TestFileSystem::testGetCurrentDirectory()
 {
-    UTEST_ASSERT(FileSystem::isFile(FileSystem::getFullPath("resources/TestFileSystem/testGetStringDir")));
+    HE_UTEST_ASSERT(FileSystem::isFile(FileSystem::getFullPath("resources/TestFileSystem/testGetStringDir")));
 }
 
 } // Test
