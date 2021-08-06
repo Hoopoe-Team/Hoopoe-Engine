3ec77383fd617f0af2298c2300041ed30cc0ab27
xojld0 <hacker1337pro@gmail.com>
2021-08-06 16:48:49 +0200

    update

--- include.cmake
+++ include.cmake
@@ -11,3 +11,29 @@ macro(he_add_test SUITE_NAME RESOURCES_BOOL)
 
     target_link_libraries(${SUITE_NAME} PRIVATE UTest)
 endmacro()
+
+macro(he_add_module PROJECT_LINK_NAME MODULE_NAME MODULE_FULL_PATH)
+    file(GLOB ${MODULE_NAME}_SOURCES
+        "${MODULE_FULL_PATH}/src/*.cpp"
+    )
+
+    if (UNIX AND NOT APPLE)
+        file(GLOB ${MODULE_NAME}_PLATFORM_SOURCES
+            "${MODULE_FULL_PATH}/src/platform/linux/*.cpp"
+        )
+    elseif(WIN32)
+        file(GLOB ${MODULE_NAME}_PLATFORM_SOURCES
+            "${MODULE_FULL_PATH}/src/platform/win/*.cpp"
+        )
+    elseif(APPLE)
+        file(GLOB ${MODULE_NAME}_PLATFORM_SOURCES
+            "${MODULE_FULL_PATH}/src/platform/mac/*.cpp"
+        )
+    endif()
+
+    target_sources(${PROJECT_LINK_NAME} PRIVATE ${${MODULE_NAME}_SOURCES} ${${MODULE_NAME}_PLATFORM_SOURCES})
+endmacro()
+
+macro(he_add_engine_module MODULE_NAME)
+    he_add_module(${ENGINE_PROJECT_NAME} ${MODULE_NAME} ${MODULE_NAME})
+endmacro()
